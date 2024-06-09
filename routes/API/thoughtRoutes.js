85504c18c9b8f0eb1e9bc4ef0c497fe11484f7e7
Amandrews <amandrews09@gmail.com>
2024-06-09 00:17:33 -0600

    ugh. why is there red in the index.js

--- routes/API/thoughtRoutes.js
+++ routes/API/thoughtRoutes.js
@@ -8,7 +8,7 @@ const {
   deleteThought,
   addReaction,
   deleteReaction,
-} = require("../../controllers/thoughtController");
+} = require('../../controllers/thoughtController');
 
 router.route("/").get(getAllThoughts).post(createThought);
 
